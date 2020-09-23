69ea90b208f02b01f3efc643fbcb07e2690b7f11
Azul <shredi@protonmail.com>
2020-09-23 12:39:34 -0500

    intial commit

--- js/main.js
+++ js/main.js
@@ -1,59 +1,6 @@
-"use strict"
+// Name: Azul Lanas
+// Date: 09/23/2020
+// Purpose: Form Validation
 
-//get the operand strings and convert to float or NaN
-function getOperands() {
-    var operands = [];
-    operands[0] = parseFloat($('#op1').val());
-    operands[1] = parseFloat($('#op2').val());
-    return operands;
-}
+"use strict";
 
-/*
- * driver function for the calculate button
- */
-function calculate() {
-    //set up variables
-    var operands = null;
-    var answer = "error";
-    operands = getOperands();
-
-    //check to see we have valid operands
-    if (!isNaN(operands[0]) && !isNaN(operands[1])) {
-        //based on the radio button checked, perform the calculation
-        switch ($('input[name=operator]:checked').val()) {
-            case "add":
-                answer = (operands[0] + operands[1]).toFixed(2);
-                break;
-            case "sub":
-                answer = (operands[0] - operands[1]).toFixed(2);
-                break;
-            case "multi":
-                answer = (operands[0] * operands[1]).toFixed(2);
-                break;
-            case "div":
-                //checks for divide by zero before dividing
-                answer = operands[1] ? (operands[0] / operands[1]).toFixed(2) : "undefined";
-                break;
-        }
-    }
-    
-    //put the answer in the answer area
-    $('#answer-area').html("The answer is: " + answer);
-}
-
-/*
- * event handler for the calculate button
- */
-$("#calcbtn").click(function () {
-    calculate();
-    return false;
-});
-
-/*
- * will execute only after the document is rendered and
- * before the user can interact with the page
- */
-$( document ).ready(function() {
-    //prepopulate the answer area
-    $('#answer-area').html("Answer Area");
-});
